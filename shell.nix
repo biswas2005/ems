782cb5882d26918a92c7542dd2782a89a4d8b381
biswas2005 <biswas2005a@gmail.com>
2026-02-16 19:14:54 +0530

    nix-shell

--- shell.nix
+++ shell.nix
@@ -1,10 +1,17 @@
-{pkgs ? import <nixpkgs> {}}:
+{ pkgs ? import <nixpkgs> {} }:
 
-pkgs.mkShell{
-    buildInputs=[
-        pkgs.go_1_26
-        pkgs.redis
-        pkgs.mysql80
-    ];
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.go_1_25
+    pkgs.docker
+    pkgs.docker-compose
+    pkgs.git
+    pkgs.mysql80
+    pkgs.redis
+  ];
 
+  shellHook = ''
+    echo "EMS Dev Environment Ready"
+    echo "Go version: $(go version)"
+  '';
 }
